96f4ad801033ac02cf7f88bde0cd77646b1c0aca
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-01-03 13:14:11 +0200

    refactor: adjust app logic

--- d1-bin2dec/index.js
+++ d1-bin2dec/index.js
@@ -1,27 +1,22 @@
-// get the inputs and validate them: should be 1s or 0s and should only be 8 digits
-// function to convert binary to decimal
-
-const input = document.querySelector('#binary')
-const output = document.querySelector('.output')
-const calculate = document.querySelector('#result')
+const input = document.querySelector('#binary');
+const output = document.querySelector('#output');
+const calculate = document.querySelector('#result');
 
 function validateInputBinaryNumbers(inputValue) {
-    const splitValue = inputValue.trim("").split("")
-    return splitValue.some(e => Number(e) === 0 || Number(e) === 1)
-}
-
-function validateInputBinaryLength(inputValue) {
-    return inputValue.length < 8 ? true : false
+    const splitValue = inputValue.split("");
+    return splitValue.every(e => e === '0' || e === '1');
 }
 
 calculate.addEventListener('click', () => {
     const inputValue = input.value;
-    output.textContent = inputValue ?? 0
-    if (validateInputBinaryNumbers(inputValue)) {
-        console.log("Input should contain only 1s and 0s")
+
+    output.textContent = '';
+
+    if (!validateInputBinaryNumbers(inputValue)) {
+        output.textContent = "Input should contain only 1s and 0s.";
+        return;
     }
 
-    if (!validateInputBinaryLength(inputValue)) {
-        console.log("Binary number should be 8")
-    }
-})
+    const decimalValue = parseInt(inputValue, 2);
+    output.textContent = `Decimal Value: ${decimalValue}`;
+});
