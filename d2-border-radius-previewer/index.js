5ad64ae420c610c4832f49fff2f7f974a10fdcf3
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-01-05 07:39:32 +0200

    feature: implemented box previewer border radius change

--- d2-border-radius-previewer/index.js
+++ d2-border-radius-previewer/index.js
@@ -4,6 +4,15 @@ const boxPreview = document.querySelector('.prev-box')
 borderInput.forEach(el => {
     el.addEventListener('change', (e) => {
         const currentInput = e.target.name
-        console.log(currentInput)
+        const currentValue = e.target.value
+        if (currentInput === "border-radius-input-top-left") {
+            boxPreview.style.borderTopLeftRadius = `${currentValue}px`
+        } else if (currentInput === "border-radius-input-top-right") {
+            boxPreview.style.borderTopRightRadius = `${currentValue}px`
+        } else if (currentInput === "border-radius-input-bottom-right") {
+            boxPreview.style.borderBottomRightRadius = `${currentValue}px`
+        } else if (currentInput === "border-radius-input-bottom-left") {
+            boxPreview.style.borderBottomLeftRadius = `${currentValue}px`
+        }
     })
 })
