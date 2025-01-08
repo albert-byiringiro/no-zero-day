3fd7bef0574a12c29f123e27cff5226b3efb2273
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-01-08 17:39:24 +0200

    feature: added displaying entered digits

--- d3-calculator/script.js
+++ d3-calculator/script.js
@@ -1,2 +1,11 @@
 const display = document.getElementById('display')
 const digits = document.querySelectorAll(".cal-digits")
+
+// Display digits clicked
+digits.forEach(digit => {
+    digit.addEventListener("click", () => {
+        display.value = display.value + digit.value
+    })
+})
+// clear displayed digits
+// calculation operations
