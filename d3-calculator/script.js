27b5ff9e311c7fc49f8dd8d41ede147d11b806f7
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-01-08 18:27:50 +0200

    feature: added an operation for addition

--- d3-calculator/script.js
+++ d3-calculator/script.js
@@ -1,6 +1,40 @@
 const display = document.getElementById('display')
 const digits = document.querySelectorAll(".cal-digits")
 const clear = document.querySelector(".cal-clear")
+const operations = document.querySelectorAll(".cal-operation")
+
+// Handler variables
+let firstCache
+let secondCache
+let summation
+let activeOperation
+
+// Helper functions
+const clearAll = () => {
+    display.value = ""
+}
+
+const addOperation = () => {
+    firstCache = parseInt(display.value)
+    activeOperation = "+"
+    clearAll()
+}
+
+const sumOperation = () => {
+    if (!firstCache || !display.value) {
+        console.log("Operation not allowed")
+        return
+    }
+
+    secondCache = parseInt(display.value)
+
+    if (activeOperation === "+") {
+        display.value = firstCache + secondCache
+
+        firstCache = display.value
+        secondCache = ""
+    }
+}
 
 // [TODO]: Display digits clicked
 digits.forEach(digit => {
@@ -14,7 +48,15 @@ digits.forEach(digit => {
 })
 
 // [TODO]: clear displayed digits
-clear.addEventListener('click', () => {
-    display.value = ""
-})
+clear.addEventListener('click', clearAll)
+
 // [TODO]: Add operation for addition
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+        if (operation.value === "+") {
+            addOperation()
+        } else if (operation.value === "=") {
+            sumOperation()
+        }
+    })
+})
