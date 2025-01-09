852685f5a4f18aaf7d5a29f586582ae9a3e021cf
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-01-09 11:43:45 +0200

    feature: added stats table header

--- d4-word-frequency/script.js
+++ d4-word-frequency/script.js
@@ -1,13 +1,13 @@
 const wordspace = document.getElementById('wordspace')
 const countBtn = document.querySelector('.btn-primary')
+const statsTable = document.querySelector('.word-stats-table')
 
 // [TODO] get the words inserted in the text area
 countBtn.addEventListener('click', () => {
     const words = wordspace.value
     const wordsArr = words.toLowerCase().split(/[\s]/g).filter(word => word !== "")
 
-    console.log(wordsArr)
-
+    // [TODO] get the occurences of each word
     let occurences = {};
     for (let i = 0; i < wordsArr.length; i++) {
         if (occurences[wordsArr[i]]) {
@@ -18,8 +18,30 @@ countBtn.addEventListener('click', () => {
     }
 
     console.log(occurences)
+
+    const fragment = document.createDocumentFragment()
+
+    const thead = document.createElement('thead')
+    const headerRow = document.createElement('tr')
+
+    const headers = ["Words", "Frequency"]
+    headers.forEach(headerText => {
+        const th = document.createElement('th')
+        th.scope = "col"
+        th.textContent = headerText
+        headerRow.appendChild(th)
+    })
+
+    thead.appendChild(headerRow)
+
+    // for (const [key, value] of Object.entries(occurences)) {
+    //     const li = document.createElement('li')
+    //     li.textContent = `${key} - ${value}`
+    //     fragment.appendChild(li)
+    // }
+
+    statsTable.appendChild(thead)
 })
-// [TODO] get the occurences of each word
 // [TODO] display the words and their count occurences
 // [TODO] display the words in the graph
 // [TODO] load word samples from an api
