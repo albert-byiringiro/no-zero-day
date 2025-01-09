2ee65590d7f1d2fa1ef25acd86be5740bb7da23b
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-01-09 12:11:11 +0200

    refactor: added helper function to generate tables

--- d4-word-frequency/script.js
+++ d4-word-frequency/script.js
@@ -4,61 +4,63 @@ const statsTable = document.querySelector('.word-stats-table')
 
 // helper functions
 
-const countWords = () => {
-    () => {
-        const words = wordspace.value
-        const wordsArr = words.toLowerCase().split(/[\s]/g).filter(word => word !== "")
+const generateTable = (occurences) => {
+    const fragment = document.createDocumentFragment()
 
-        // [TODO] get the occurences of each word
-        let occurences = {};
-        for (let i = 0; i < wordsArr.length; i++) {
-            if (occurences[wordsArr[i]]) {
-                occurences[wordsArr[i]]++;
-            } else {
-                occurences[wordsArr[i]] = 1;
-            }
-        }
+    const thead = document.createElement('thead')
+    const headerRow = document.createElement('tr')
 
-        const fragment = document.createDocumentFragment()
+    const headers = ["Words", "Frequency"]
+    headers.forEach(headerText => {
+        const th = document.createElement('th')
+        th.scope = "col"
+        th.textContent = headerText
+        headerRow.appendChild(th)
+    })
 
-        const thead = document.createElement('thead')
-        const headerRow = document.createElement('tr')
+    thead.appendChild(headerRow)
 
-        const headers = ["Words", "Frequency"]
-        headers.forEach(headerText => {
-            const th = document.createElement('th')
-            th.scope = "col"
-            th.textContent = headerText
-            headerRow.appendChild(th)
-        })
+    const tbody = document.createElement('tbody')
 
-        thead.appendChild(headerRow)
+    for (const [word, frequency] of occurences) {
+        const row = document.createElement('tr')
 
-        const tbody = document.createElement('tbody')
+        const tdWord = document.createElement('th')
+        tdWord.scope = "row"
+        tdWord.textContent = word
 
-        const sortedOcurrencies = Object.entries(occurences).sort((a, b) => b[1] - a[1])
+        const tdFrequency = document.createElement('td')
+        tdFrequency.textContent = frequency
 
-        for (const [word, frequency] of sortedOcurrencies) {
-            const row = document.createElement('tr')
+        row.appendChild(tdWord)
+        row.appendChild(tdFrequency)
 
-            const tdWord = document.createElement('th')
-            tdWord.scope = "row"
-            tdWord.textContent = word
-
-            const tdFrequency = document.createElement('td')
-            tdFrequency.textContent = frequency
-
-            row.appendChild(tdWord)
-            row.appendChild(tdFrequency)
-
-            tbody.appendChild(row)
-        }
-
-        fragment.appendChild(thead)
-        fragment.appendChild(tbody)
-
-        statsTable.appendChild(fragment)
+        tbody.appendChild(row)
     }
+
+    fragment.appendChild(thead)
+    fragment.appendChild(tbody)
+
+    statsTable.appendChild(fragment)
+}
+
+const countWords = () => {
+    const words = wordspace.value
+    const wordsArr = words.toLowerCase().split(/[\s]/g).filter(word => word !== "")
+
+    // [TODO] get the occurences of each word
+    let occurences = {};
+    for (let i = 0; i < wordsArr.length; i++) {
+        if (occurences[wordsArr[i]]) {
+            occurences[wordsArr[i]]++;
+        } else {
+            occurences[wordsArr[i]] = 1;
+        }
+    }
+
+    const sortedOcurrencies = Object.entries(occurences).sort((a, b) => b[1] - a[1])
+
+    generateTable(sortedOcurrencies)
 }
 // [TODO] get the words inserted in the text area
 countBtn.addEventListener('click', countWords)
