532d30293e6ff1c20bf0504a418321b1cf2edd8f
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-02-24 17:14:06 +0200

    feat: added Pizza component made responsive

--- framework-playground/react/src/App.js
+++ framework-playground/react/src/App.js
@@ -1,7 +1,7 @@
-const Pizza = () => {
+const Pizza = ({ name, description }) => {
     return React.createElement('div', {}, [
-        React.createElement('h2', {}, 'The Pepperoni Pizza'),
-        React.createElement('p', {}, 'Mozzarella cheese'),
+        React.createElement('h2', {}, name),
+        React.createElement('p', {}, description),
     ])
 }
 
@@ -11,9 +11,18 @@ const App = () => {
         {},
         [
             React.createElement("h1", {}, "Pixel Perfect Pizzas"),
-            React.createElement(Pizza),
-            React.createElement(Pizza),
-            React.createElement(Pizza),
+            React.createElement(Pizza, {
+                name: "The Pepperoni Pizza",
+                description: "Mozarella Cheese, Pepperoni",
+            }),
+            React.createElement(Pizza, {
+                name: 'The Hawaiian Pizza',
+                description: "Sliced Ham, Pineapple, Mozzarella"
+            }),
+            React.createElement(Pizza, {
+                name: "The Big Meat Pizza",
+                description: "Bacon, Pepperoni, Italian sausage"
+            }),
         ]
     );
 };
